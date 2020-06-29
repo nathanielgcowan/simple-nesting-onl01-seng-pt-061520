3efcfef1c4917944307bb3eeeb050588d2ee0cd3
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-29 17:47:42 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,20 +56,21 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+  # add the following information to the top level of programmer_hash
+  # :yukihiro_matsumoto => {
+  #   :known_for => "Ruby",
+  #    :languages => ["LISP", "C"]
+  # }
 
 	programmer_hash = 
- 		{
-        :grace_hopper => {
+ 		{  
+      :grace_hopper => {
           :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
+          :languages => ["COBOL", "FORTRAN"] 
         },
         :alan_kay => {
           :known_for => "Object Orientation",
@@ -79,8 +81,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+     programmer_hash[:yukihiro_matsumoto] = {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      }
+     
+     return programmer_hash
+     
 end
 
 def changing_alan
@@ -101,8 +108,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +128,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages]<<'Assembly'
 
+return programmer_hash
 end
